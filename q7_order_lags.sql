a1824b8bf9c5619bc7582a71416adc857a6a0a90
liu-peng-xi <114712993+liu-peng-xi@users.noreply.github.com>
2022-10-07 15:20:30 +0800

    Delete q7_order_lags.sql

--- q7_order_lags.sql
+++
@@ -1,6 +0,0 @@
-SELECT Id, OrderDate, PreviousOrderDate, ROUND(julianday(OrderDate) - julianday(PreviousOrderDate), 2) AS Difference
-FROM (SELECT Id, OrderDate, LAG(OrderDate, 1, OrderDate) OVER (ORDER BY OrderDate ASC) AS PreviousOrderDate
-FROM "Order"
-WHERE CustomerId = 'BLONP'
-ORDER BY OrderDate ASC
-LIMIT 10);
