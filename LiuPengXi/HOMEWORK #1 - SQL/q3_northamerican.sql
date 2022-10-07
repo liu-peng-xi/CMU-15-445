5f18e7a699fa8c60ef2df0f451ced7a74fbe18ca
liu-peng-xi <114712993+liu-peng-xi@users.noreply.github.com>
2022-10-07 15:28:27 +0800

    Delete LiuPengXi/HOMEWORK #1 - SQL directory

--- LiuPengXi/HOMEWORK #1 - SQL/q3_northamerican.sql
+++
@@ -1,7 +0,0 @@
-SELECT Id, ShipCountry,
-CASE WHEN ShipCountry IN ('USA', 'Mexico','Canada') THEN 'NorthAmerica'
-ELSE 'OtherPlace' END AS Area
-FROM "Order" 
-WHERE Id >= 15445 
-ORDER BY Id ASC
-limit 20;
